1b9a6866fbced3cd2df43aadd6516f8211da1abd
Federico Asara <federico.asara@gmail.com>
2023-11-24 18:59:11 +0000

    Make plugin more resilient to race conditions
    - Use a temporary file for formatting
    - Prevent out-of-order formatting
    - Track folded lines where possible
    - Add documentation

--- addons/format_on_save/format_on_save.gd
+++ addons/format_on_save/format_on_save.gd
@@ -1,66 +1,138 @@
 @tool
 class_name FormatOnSave extends EditorPlugin
 
-const SUCCESS: int = 0
-const AUTO_RELOAD_SETTING: String = "text_editor/behavior/files/auto_reload_scripts_on_external_change"
-var original_auto_reload_setting: bool
+## gdformat exit code returned on a successful format.
+const SUCCESS := 0
+
+#region Lifecycle events
 
 
-# LIFECYCLE EVENTS
 func _enter_tree():
-	activate_auto_reload_setting()
 	resource_saved.connect(on_resource_saved)
 
 
 func _exit_tree():
 	resource_saved.disconnect(on_resource_saved)
-	restore_original_auto_reload_setting()
 
 
-# CALLED WHEN A SCRIPT IS SAVED
-func on_resource_saved(resource: Resource):
-	if resource is Script:
-		var script: Script = resource
-		var filepath: String = ProjectSettings.globalize_path(resource.resource_path)
+## Signal handler for [member resource_saved].
+func on_resource_saved(resource: Resource) -> void:
+	if not resource is Script:
+		return
+	format(resource as Script)
 
-		# Run gdformat
-		var exit_code = OS.execute("gdformat", [filepath])
 
-		# Replace source_code with formatted source_code
-		if exit_code == SUCCESS:
-			var formatted_source = FileAccess.get_file_as_string(resource.resource_path)
-			var text_edit: CodeEdit = (
-				get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
-			)
-			FormatOnSave.reload_script(text_edit, formatted_source)
+#endregion
+
+#region Out-of-order formatting prevention
+
+## Stores instances of [Token] by their [member Token.resource_path].
+##
+## Must be accessed after locking [member mutex].
+var tokens_by_resource_path := {}
+
+## Protects access to [member tokens_by_resource_path].
+var mutex := Mutex.new()
+
+
+## Tracks when a format on save operation started by its [member Token.value] property.
+##
+## A token is maintained for all scripts in [member tokens_by_resource_path]. After creating a copy
+## of the token for a script, it's possible to check if further saves have been performed by
+## comparing the [member Token.value]s of the copy and the one in [member tokens_by_resource_path].
+## If they differ, then the codepath that has the token should not save the file since the version
+## of the script it formatted has been superseded by a new one.
+class Token:
+	var value: int
+	var resource_path: String
+
+	func _init(value_: int, resource_path_: String) -> void:
+		self.value = value_
+		self.resource_path = resource_path_
+
+	func copy() -> Token:
+		return Token.new(value, resource_path)
+
+
+## Acquires a token for [param script] and returns it.
+func acquire_token(script: Script) -> Token:
+	mutex.lock()
+
+	var resource_path := script.resource_path
+	var token := (
+		tokens_by_resource_path[resource_path] as Token
+		if tokens_by_resource_path.has(resource_path)
+		else Token.new(0, resource_path)
+	)
+	token.value += 1
+	tokens_by_resource_path[resource_path] = token
+
+	mutex.unlock()
+	# Return a copy of the token so that concurrent modifications to token do not propagate to the
+	# token returned to and held by the caller.
+	return token.copy()
+
+
+## Executes [param body] if [param token] has the same value as the corresponding [class Token] in
+## [member tokens_by_resource_path].
+func if_token_valid(token: Token, body: Callable) -> bool:
+	mutex.lock()
+	var existing_token := tokens_by_resource_path[token.resource_path] as Token
+	var is_token_valid := existing_token.value == token.value
+	if is_token_valid:
+		body.call()
+	mutex.unlock()
+	return is_token_valid
+
+
+#endregion
+
+
+## Formats [param script] using gdformat.
+func format(script: Script) -> void:
+	var token = acquire_token(script)
+
+	# Create a temporary copy of the script. Take the token into account to avoid race conditions
+	# between the editor saving the same script again and the formatter running.
+	var source := script.resource_path
+	var temp := "%s.%d.temp" % [source, token.value]
+	DirAccess.copy_absolute(source, temp)
+
+	# Run the formatter and check for its exit code.
+	var exit_code := OS.execute("gdformat", [ProjectSettings.globalize_path(temp)])
+	if exit_code == SUCCESS:
+		var formatted_source := FileAccess.get_file_as_string(temp)
+		var save_successful = if_token_valid(token, func(): reload_script(formatted_source))
+		if not save_successful:
+			push_warning("format_on_save: found new revision of %s after formatting")
+	else:
+		push_error("format_on_save: encountered error %d while formatting %s" % [exit_code, source])
+
+	DirAccess.remove_absolute(temp)
 
 
 # Workaround until this PR is merged:
 # https://github.com/godotengine/godot/pull/83267
 # Thanks, @KANAjetzt 💖
-static func reload_script(text_edit: TextEdit, source_code: String) -> void:
-	var column := text_edit.get_caret_column()
-	var row := text_edit.get_caret_line()
-	var scroll_position_h := text_edit.get_h_scroll_bar().value
-	var scroll_position_v := text_edit.get_v_scroll_bar().value
+func reload_script(source_code: String) -> void:
+	var code_edit := (
+		get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
+		as CodeEdit
+	)
 
-	text_edit.text = source_code
-	text_edit.set_caret_column(column)
-	text_edit.set_caret_line(row)
-	text_edit.scroll_horizontal = scroll_position_h
-	text_edit.scroll_vertical = scroll_position_v
+	var column := code_edit.get_caret_column()
+	var row := code_edit.get_caret_line()
+	var scroll_position_h := code_edit.get_h_scroll_bar().value
+	var scroll_position_v := code_edit.get_v_scroll_bar().value
+	var folded_lines := code_edit.get_folded_lines()
 
-	text_edit.tag_saved_version()
+	code_edit.text = source_code
+	code_edit.set_caret_column(column)
+	code_edit.set_caret_line(row)
+	code_edit.scroll_horizontal = scroll_position_h
+	code_edit.scroll_vertical = scroll_position_v
+	for line in folded_lines:
+		if code_edit.can_fold_line(line):
+			code_edit.fold_line(line)
 
-
-# For this workaround to work, we need to disable the "Reload/Resave" pop-up
-func activate_auto_reload_setting():
-	var settings := get_editor_interface().get_editor_settings()
-	original_auto_reload_setting = settings.get(AUTO_RELOAD_SETTING)
-	settings.set(AUTO_RELOAD_SETTING, true)
-
-
-# If the plugin is disabled, let's attempt to restore the original editor setting
-func restore_original_auto_reload_setting():
-	var settings := get_editor_interface().get_editor_settings()
-	settings.set(AUTO_RELOAD_SETTING, original_auto_reload_setting)
+	code_edit.tag_saved_version()
